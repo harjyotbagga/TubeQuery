8ace9f9bf580a4a59e2fcfd966c0914dca9086bc
harjyotbagga <imharjyotbagga@gmail.com>
2022-06-15 01:06:31 +0530

    Adding Redis as Worker Broker Backend

--- worker/celery_app.py
+++ worker/celery_app.py
@@ -6,6 +6,7 @@ RABBITMQ_USERNAME = os.getenv("RABBITMQ_USERNAME", "guest")
 RABBITMQ_PASSWORD = os.getenv("RABBITMQ_PASSWORD", "guest")
 RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", "localhost")
 RABBITMQ_PORT = os.getenv("RABBITMQ_PORT", "5672")
+REDIS_BACKEND_LINK = os.getenv("REDIS_BACKEND_LINK", "redis://localhost:6379/0")
 
 RABBITMQ_BROKER_LINK = (
     f"amqp://{RABBITMQ_USERNAME}:{RABBITMQ_PASSWORD}@{RABBITMQ_HOST}:{RABBITMQ_PORT}//"
@@ -21,8 +22,7 @@ config = {
     "timezone": pytz.timezone("UTC"),
 }
 
-# TODO: Add a backend link to the config
-app = Celery("tube_main", broker=RABBITMQ_BROKER_LINK)
+app = Celery("tube_main", broker=RABBITMQ_BROKER_LINK, backend=REDIS_BACKEND_LINK)
 app.conf.update(**config)
 
 # celery -A tasks worker -l info
